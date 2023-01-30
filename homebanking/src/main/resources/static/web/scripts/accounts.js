88cceec95361808a41a79fdf2dd2114b42bee9f8
Juan-Bianchi <juanbianchi@icloud.com>
2023-01-30 13:34:08 -0300

    se agregan funcionalidades del front y diseño de accounts.html

---
+++ homebanking/src/main/resources/static/web/scripts/accounts.js
@@ -0,0 +1,40 @@
+const {createApp} = Vue;
+
+createApp( {
+    data(){
+        return{
+            client: undefined,
+            windowWidth: window.innerWidth,
+            accounts: [],
+
+        }
+    },
+
+    created() {
+        this.loadData();
+    },
+
+    mounted() {
+        window.addEventListener('resize', this.onResize);
+    },
+
+    methods: {
+        loadData(){
+            axios.get("http://localhost:8080/api/clients/1")
+                 .then(response => {
+                    this.client = {... response.data};
+                    console.log(this.client);
+                    this.accounts = this.client.accounts.map(account => account);
+                 })
+                 .catch(err => console.error(err.message));
+        },
+
+        onResize(event) {
+            this.windowWidth = screen.width
+        },
+
+
+
+    },
+
+}).mount("#app");
