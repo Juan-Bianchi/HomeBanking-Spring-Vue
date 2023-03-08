222bebdd560ba3e07ef8be4001913a0ff20ee2de
unknown <juanbianchi@icloud.com>
2023-03-08 19:39:23 -0300

    task 9 terminada

--- homebanking/src/main/resources/static/web/scripts/loan-application.js
+++ homebanking/src/main/resources/static/web/scripts/loan-application.js
@@ -6,13 +6,14 @@ createApp({
         return{
             client: undefined,
             loans: [],
+            accounts: [],
             genericLoans: [],
             windowWidth: window.innerWidth,
             activeChecks: ["Mortgage", "Personal", "Automotive"],
             filteredLoans: [],
             typeOfLoans: [],
-            totalPages: undefined,
-            pageNumber: undefined,
+            totalPages: 1,
+            pageNumber: 1,
             visibleLoans: undefined,
             navNumbersArray: [],
             currentNavModulus: 0,
@@ -20,6 +21,13 @@ createApp({
             visibleNavNumbers: [],
             emptyLinesAmount: undefined,
             availableAmounts: [],
+            stringAmount: "",
+            loanApplicationDTO: {
+                idLoan: undefined,
+                amount: undefined,
+                payments: undefined,
+                associatedAccountNumber: undefined,
+            }
         }
     },
 
@@ -35,12 +43,13 @@ createApp({
         loadData: function(){
             let ownLoans = axios.get('http://localhost:8080/api/clients/current');
             let genericLoans = axios.get('http://localhost:8080/api/loans');
-            Promise.all([ownLoans, genericLoans])
+            let ownAccounts = axios.get('http://localhost:8080/api/clients/current/accounts')
+            Promise.all([ownLoans, genericLoans, ownAccounts])
                    .then(response => {    
                         this.client = {... response[0].data};
                         this.loans = this.client.loans.map(loan => ({... loan})).sort((l1, l2) => (l1.id > l2.id ? 1: -1));
-                        console.log(this.loans);
-                        this.genericLoans = [... response[1].data].sort((l1, l2)=> (l1.id - l2.id));
+                        this.genericLoans = [... response[1].data].sort((l1, l2)=> l1.id - l2.id)
+                        this.accounts = [... response[2].data].sort((a1, a2)=> a1.id - a2.id)
                         this.manageData();
                    })
                    //.catch(err => console.error(err.message));
@@ -65,20 +74,34 @@ createApp({
             let mortgage = this.loans.filter(loan => loan.name.toLowerCase().includes('mortgage')).reduce(((total, loan)=> total + loan.amount),0);
             let personal = this.loans.filter(loan => loan.name.toLowerCase().includes('personal')).reduce(((total, loan)=> total + loan.amount),0);
             let automotive = this.loans.filter(loan => loan.name.toLowerCase().includes('automotive')).reduce(((total, loan)=> total + loan.amount),0);
+            let availableMort = this.genericLoans.find(loan=> loan.name.includes('Mortgage')).maxAmount - mortgage;
+            let availablePers = this.genericLoans.find(loan=> loan.name.includes('Personal')).maxAmount - personal;
+            let availableAuto = this.genericLoans.find(loan=> loan.name.includes('Automotive')).maxAmount - automotive;
 
-            console.log(mortgage, personal, automotive);
-            this.availableAmounts.push(({
-                name: 'Mortgage',
-                available: this.genericLoans.find(loan=> loan.name.includes('Mortgage')).maxAmount - mortgage,
-            }))
-            this.availableAmounts.push(({
-                name: 'Personal',
-                available: this.genericLoans.find(loan=> loan.name.includes('Personal')).maxAmount - personal,
-            }))
-            this.availableAmounts.push(({
-                name: 'Automotive',
-                available: this.genericLoans.find(loan=> loan.name.includes('Automotive')).maxAmount - automotive,
-            }))
+            if(availableMort > 1){
+                this.availableAmounts.push(({
+                    id: this.genericLoans.find(loan=> loan.name.includes('Mortgage')).id,
+                    name: 'Mortgage',
+                    available: availableMort,
+                }))
+            }
+            
+            if(availablePers > 1){
+                this.availableAmounts.push(({
+                    id: this.genericLoans.find(loan=> loan.name.includes('Personal')).id,
+                    name: 'Personal',
+                    available: availablePers,
+                }))
+            }
+            
+            if(availableAuto > 1){
+                this.availableAmounts.push(({
+                    id: this.genericLoans.find(loan=> loan.name.includes('Automotive')).id,
+                    name: 'Automotive',
+                    available: availableAuto,
+                }))
+            }
+            
         },
 
 
@@ -112,7 +135,8 @@ createApp({
         
         changePage: function(movement){
             this.pageNumber += movement;
-            this.currentNavModulus = (this.pageNumber - 1) / 3;
+            this.currentNavModulus = Math.ceil((this.pageNumber - 1) / 3);
+            console.log(this.currentNavModulus);
             this.renderLoans();
         },
 
@@ -121,8 +145,41 @@ createApp({
             this.renderLoans();
         },
 
+        formatAmountInput: function(){
+            if(Number.isNaN(Number(this.stringAmount.slice(3)))){                
+                this.stringAmount = "";
+                this.loanApplicationDTO.amount = 0;
+            }
+            else{
+                console.log(this.stringAmount);
+                this.loanApplicationDTO.amount = this.stringAmount;
+                this.stringAmount =  Number(this.stringAmount).toLocaleString("es-AR", {style:"currency",currency:"USD"});
+            }
+            
+        },
+
+        numberWhenUsing: function(){
+            this.stringAmount = "";         
+        },
+
         onResize(event) {
             this.windowWidth = screen.width
+        },
+
+        applyLoan: function(){
+            axios.post('/api/loans', { idLoan: this.loanApplicationDTO.idLoan, amount: this.loanApplicationDTO.amount, payments: this.loanApplicationDTO.payments, associatedAccountNumber: this.loanApplicationDTO.associatedAccountNumber,})
+                 .then(response => {
+                    window.location.href = "http://localhost:8080/web/loan-application.html?";
+                 })
+                 .catch(err =>{
+                    console.log([err])
+        
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: `The loan application was not completed: ${err.message.includes('403')? err.response.data: "There was an error in the loan request."}`,
+                    })
+                 })
         },
 
         logout(){
