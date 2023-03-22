31bd11281dad7b061a47a7df829abe67fb4dcf77
unknown <juanbianchi@icloud.com>
2023-03-22 11:28:53 -0300

    final

--- homebanking/src/main/java/com/mindhub/homebanking/configurations/WebAuthentication.java
+++ homebanking/src/main/java/com/mindhub/homebanking/configurations/WebAuthentication.java
@@ -26,6 +26,7 @@ public class WebAuthentication extends GlobalAuthenticationConfigurerAdapter {
     @Autowired
     ClientRepository clientRepository;
 
+    
     @Override
     public void init(AuthenticationManagerBuilder auth) throws Exception {
 
